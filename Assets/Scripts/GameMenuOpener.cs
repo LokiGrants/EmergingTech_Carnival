c22b94cdaed074529a2d343b505e3147072e72eb
Pedro Loures <pedro.loures@live.com>
2021-02-24 18:41:35 -0500

    First tests for in-game menu

---
+++ Assets/Scripts/GameMenuOpener.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuOpener : MonoBehaviour
+{
+    public GameObject canvas;
+    public float countdownTotalTime = 1f;
+
+    private bool turnedOn;
+    private Coroutine countdownCoroutine;
+
+    public void StartCountDown()
+    {
+        if (!turnedOn)
+        {
+            countdownCoroutine = StartCoroutine(CountToTurnOn());
+        }
+    }
+
+    public void StopCountDown()
+    {
+        StopCoroutine(countdownCoroutine);
+        countdownCoroutine = null;
+    }
+
+    IEnumerator CountToTurnOn()
+    {
+        yield return new WaitForSecondsRealtime(countdownTotalTime);
+        turnedOn = true;
+        canvas.SetActive(true);
+    }
+
+    public void CloseWindow()
+    {
+        turnedOn = false;
+        canvas.SetActive(false);
+    }
+}
