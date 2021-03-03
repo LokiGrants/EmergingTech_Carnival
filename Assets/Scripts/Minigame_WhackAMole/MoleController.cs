6a1d5ac9b279dbe31fc23c5a7c1f9b3d3fd02e3b
Pedro Loures <pedro.loures@live.com>
2021-03-03 01:06:22 -0500

    First prototype for Whack A Mole
    If you want to test it, please open the Whack A Mole scene, press play.
    
    Then click Game Manager on the inspector > Right click and select Start Whacka on the context menu.
    
    For now you can only watch the game play itself - the code for hammer hit and what the mole should do is there already. (For now it instantly returns to underground, we have to talk what we wanna do later (polishing)

---
+++ Assets/Scripts/Minigame_WhackAMole/MoleController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleController : MonoBehaviour
+{
+    [HideInInspector]
+    public bool canNotPlay;
+    [HideInInspector]
+    public float mole_yPosAboveGround;
+    [HideInInspector]
+    public float mole_yPosUnderGround;
+    [HideInInspector]
+    public float mole_movementTime;
+    [HideInInspector]
+    public float mole_minTimeUp;
+    [HideInInspector]
+    public float mole_maxTimeUp;
+
+    public void MoveMole()
+    {
+        StartCoroutine(MoveUpAndDownObject());
+    }
+
+    public IEnumerator MoveUpAndDownObject()
+    {
+        canNotPlay = true;
+        Coroutine coroutine = StartCoroutine(
+            MoveObject(transform.position, 
+            new Vector3(transform.position.x, mole_yPosAboveGround, transform.position.z)));
+        yield return new WaitUntil(() => transform.position.y >= mole_yPosAboveGround);
+        StopCoroutine(coroutine);
+
+        yield return new WaitForSeconds(
+            Mathf.FloorToInt(
+                Random.Range(mole_minTimeUp * 1000000, mole_maxTimeUp * 1000000) / 1000000));
+
+        coroutine = StartCoroutine(
+            MoveObject(transform.position, 
+            new Vector3(transform.position.x, mole_yPosUnderGround, transform.position.z)));
+        yield return new WaitUntil(() => transform.position.y <= mole_yPosUnderGround);
+        canNotPlay = false;
+    }
+
+    public IEnumerator MoveObject(Vector3 initialPos, Vector3 finalPos)
+    {
+        float timeElapsed = 0;
+
+        while (timeElapsed < mole_movementTime)
+        {
+            float newY = Mathf.Lerp(initialPos.y, finalPos.y, timeElapsed / mole_movementTime);
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.position = finalPos;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "HammerTime")
+        {
+            StopAllCoroutines();
+            canNotPlay = false;
+            //Particles
+            //Insta move down
+            transform.position = new Vector3(transform.position.x, mole_yPosUnderGround, transform.position.z);
+            WhackMole_Manager.Instance.OnMoleHit();
+        }
+    }
+}
---
+++ Assets/Scripts/Minigame_WhackAMole/WhackMole_Manager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WhackMole_Manager : Singleton<WhackMole_Manager>
+{
+    public float timeOfGameplayInSeconds;
+    public List<MoleController> moles;
+    public float minTimeBetweenMole = 2f;
+    public float maxTimeBetweenMole = 5f;
+    public float stepTime = 0.1f;
+
+    public float mole_yPosAboveGround;
+    public float mole_yPosUnderGround;
+    public float mole_movementTime;
+    public float mole_minTimeUp;
+    public float mole_maxTimeUp;
+
+    private int whacks;
+    private float timeForNextMole = 0;
+
+    [ContextMenu("Start Whacka")]
+    void StartMinigame()
+    {
+        whacks = 0;
+
+        foreach (MoleController mc in moles)
+        {
+            mc.mole_yPosAboveGround = mole_yPosAboveGround;
+            mc.mole_yPosUnderGround = mole_yPosUnderGround;
+            mc.mole_movementTime = mole_movementTime;
+            mc.mole_minTimeUp = mole_minTimeUp;
+            mc.mole_maxTimeUp = mole_maxTimeUp;
+        }
+
+        Debug.Log("Give VR hammer to player here");
+        StartCoroutine(MiniGameController(timeOfGameplayInSeconds));
+    }
+
+    IEnumerator MiniGameController(float totalGameTime)
+    {
+        while (totalGameTime > 0)
+        {
+            Debug.Log("Time Left " + Mathf.Floor(totalGameTime));
+            if (timeForNextMole <= 0)
+            {
+                timeForNextMole = Mathf.FloorToInt(Random.Range(minTimeBetweenMole * 1000000, maxTimeBetweenMole * 1000000) / 1000000);
+                var moleSubset = moles.Where(x => !x.canNotPlay).ToList();
+                if (moleSubset.Count > 0)
+                {
+                    moleSubset[Mathf.FloorToInt(Random.Range(0, moleSubset.Count * 1000000) / 1000000)].MoveMole();
+                }
+            }
+
+            yield return new WaitForSeconds(stepTime);
+            timeForNextMole -= stepTime;
+            totalGameTime -= stepTime;
+        }
+
+        Debug.Log("Total hits: " + whacks);
+    }
+
+    public void OnMoleHit()
+    {
+        whacks += 1;
+    }
+}
