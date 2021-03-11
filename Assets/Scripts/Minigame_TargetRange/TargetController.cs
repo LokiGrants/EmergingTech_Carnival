0c526218ae4db6c7a5f6b75d3725575bb42ddcfb
Pedro Loures <pedro.loures@live.com>
2021-03-10 19:18:36 -0500

    Few changes to Target Range, turned it into archery using the already done longbow from Steam VR.
    Included a few changes to start the minigame.

---
+++ Assets/Scripts/Minigame_TargetRange/TargetController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "projectile")
+        {
+            Debug.Log("HIT");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Minigame_TargetRange/TargetRangeManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetRangeManager : MiniGameManager<TargetRangeManager>
+{
+    public List<Transform> spawnParents;
+    private float score;
+
+    [ContextMenu("Start Target Range")]
+    void StartTargetRange()
+    {
+        score = 0;
+
+        Debug.Log("Give VR hammer to player here");
+        StartMinigame();
+    }
+}
