349b123dc24177ebaef4f29fd09355a74c188863
TylerFont <tylerfont@rogers.com>
2021-04-09 16:12:47 -0400

    Made In Game Menu
    Made In Game Menu and Settings Screen

--- Assets/Scripts/TylerScriptFolder/AudioManager.cs
+++ Assets/Scripts/TylerScriptFolder/AudioManager.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float speed;
+	private Rigidbody rb;
+	private int count;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public AudioSource musicSource;
+	public AudioSource sfxSource;
+	public AudioClip winMusic;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Pick Up"))
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+
+			sfxSource.pitch = Random.Range(0.9f, 1.9f);
+			sfxSource.Play();
+		}
+	}
+
+	void SetCountText()
+	{
+		if (count >= 12)
+		{
+			musicSource.clip = winMusic;
+			musicSource.Play();
+		}
+	}
+
 }
