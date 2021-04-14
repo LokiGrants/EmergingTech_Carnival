28453faaa6dc4506b2f742fee08238af572997c7
TylerFont <tylerfont@rogers.com>
2021-04-14 11:09:01 -0400

    Made Audio Manager Script
    Music should work in every level.

--- Assets/Scripts/TylerScriptFolder/AudioManager.cs
+++ Assets/Scripts/TylerScriptFolder/AudioManager.cs
@@ -1,52 +1,107 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+	public string name;
+	public AudioClip clip;
+
+	[Range(0f, 1f)]
+	public float volume = 0.7f;
+	//[Range(0.5f, 1.5f)]
+	//public float pitch = 1f;
+
+	//[Range(0f, 0.5f)]
+	//public float randomVolume = 0.1f;
+	//[Range(0f, 0.5f)]
+	//public float randomPitch = 0.1f;
+
+	public bool loop = false;
+
+	private AudioSource source;
+
+	public void SetSource(AudioSource _source)
+	{
+		source = _source;
+		source.clip = clip;
+		source.loop = loop;
+	}
+
+	public void Play()
+	{
+		//source.volume = volume * (1 + Random.Range(-randomVolume / 2f, randomVolume / 2f));
+		//source.pitch = pitch * (1 + Random.Range(-randomPitch / 2f, randomPitch / 2f));
+		source.Play();
+	}
+
+	public void Stop()
+	{
+		source.Stop();
+	}
+}
 
 public class AudioManager : MonoBehaviour
 {
-	public float speed;
-	private Rigidbody rb;
-	private int count;
+	public static AudioManager instance;
 
-	public AudioSource musicSource;
-	public AudioSource sfxSource;
-	public AudioClip winMusic;
+	[SerializeField]
+	Sound[] sounds;
+
+	void Awake()
+	{
+		if (instance != null)
+		{
+			if (instance != this)
+			{
+				Destroy(this.gameObject);
+			}
+		}
+		else
+		{
+			instance = this;
+			DontDestroyOnLoad(this);
+		}
+	}
 
 	void Start()
 	{
-		rb = GetComponent<Rigidbody>();
-		count = 0;
-		SetCountText();
-	}
-
-	void FixedUpdate()
-	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-		rb.AddForce(movement * speed);
-	}
-
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.gameObject.CompareTag("Pick Up"))
+		for (int i = 0; i < sounds.Length; i++)
 		{
-			other.gameObject.SetActive(false);
-			count = count + 1;
-			SetCountText();
-
-			sfxSource.pitch = Random.Range(0.9f, 1.9f);
-			sfxSource.Play();
+			GameObject _go = new GameObject("Sound_" + i + "_" + sounds[i].name);
+			_go.transform.SetParent(this.transform);
+			sounds[i].SetSource(_go.AddComponent<AudioSource>());
 		}
+
+		PlaySound("Music");
 	}
 
-	void SetCountText()
+	public void PlaySound(string _name)
 	{
-		if (count >= 12)
+		for (int i = 0; i < sounds.Length; i++)
 		{
-			musicSource.clip = winMusic;
-			musicSource.Play();
+			if (sounds[i].name == _name)
+			{
+				sounds[i].Play();
+				return;
+			}
 		}
+
+		// no sound with _name
+		Debug.LogWarning("AudioManager: Sound not found in list, " + _name);
 	}
 
+	public void StopSound(string _name)
+	{
+		for (int i = 0; i < sounds.Length; i++)
+		{
+			if (sounds[i].name == _name)
+			{
+				sounds[i].Stop();
+				return;
+			}
+		}
+
+		// no sound with _name
+		Debug.LogWarning("AudioManager: Sound not found in list, " + _name);
+	}
 }
